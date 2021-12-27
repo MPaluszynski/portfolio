d6ecf9b4832f642bd8a7f35f95f2203cd36fdca1
Maciej Paluszynski <macpalus@gmail.com>
2021-12-27 23:34:53 +0100

    Implementing basic styling of AboutMe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles/app-styles.css";
 
 import { Home } from "./components/home/Home.jsx";
-import { ParticlesBackground } from "./components/particlesBackground/ParticlesBackground.jsx";
+import { ParticlesBackground } from "./components/_reusableComponents/particlesBackground/ParticlesBackground.jsx";
 import { AboutMe } from "./components/aboutMe/AboutMe.jsx";
 
 function App() {
--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -2,6 +2,7 @@ import "./about-styling.css";
 import { aboutHighlightsArray } from "../../assets/data/arrays";
 import { aboutMeDescription } from "../../assets/data/variables";
 import { Heading } from "../_reusableComponents/heading/Heading.jsx";
+import { Buttons } from "../_reusableComponents/buttons/Buttons";
 
 export const AboutMe = () => {
   const renderedArray = aboutHighlightsArray.map((highlight) => {
@@ -10,29 +11,18 @@ export const AboutMe = () => {
 
   return (
     <div className="about-me">
-      <Heading title={"About Me"} description={"Why Choose Me"} />;
+      <Heading title={"About Me"} description={"Why Choose Me"} />
       <div className="about-me__card">
-        <>
-          <div className="about-me__card--photo"></div>
-          <div className="about-me__card--details">
-            <div className="about-me__card--details--description">
-              {aboutMeDescription}
-            </div>
-            <div className="about-me__card--details--highlights">
-              {renderedArray}
-            </div>
+        <div className="about-me__card--photo"></div>
+        <div className="about-me__card--details">
+          <div className="about-me__card--details--description">
+            {aboutMeDescription}
           </div>
-        </>
-        <div className="profile__details--options">
-          <button className="btn primary-btn">Hire Me</button>
-          <a
-            href={"files/cvpl.pdf"}
-            target={"_blank"}
-            download={"cvpl.pdf"}
-            rel={"noreferrer"}
-          >
-            <button className="btn highlighted-btn">Get Resume</button>
-          </a>
+          <div className="about-me__card--details--highlights">
+            <span>Here are a few highlights</span>
+            {renderedArray}
+          </div>
+          <Buttons />
         </div>
       </div>
     </div>
